6f2e0b7fbc4425c523fd5be6a49cecce0ce60b05
provash10 <provash20cb@gmail.com>
2024-11-26 01:58:04 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,64 @@
+
+// function sum (){
+//     let x = 10;
+//     let y = 120;
+
+//     let result = x+y;
+//     return result;
+// }
+// let sumation = sum ()
+
+// // console.log(sum());
+// console.log(sumation);
+// document.getElementById("result").innerHTML ='The result is = ' +  sumation;
+
+// ===========
+
+function sum (){
+    let x = document.getElementById("number1").value; //1200
+    let y = document.getElementById("number2").value; //800
+    
+    // console.log(x);
+    // console.log(y);
+    // let x = 80;
+    // let y = 80;
+    parseFloat(x);
+    parseFloat(y);
+    // parseInt()
+    // let result = x+y;
+    let result = parseFloat(x) + parseFloat(y);
+    // return result;
+    document.getElementById("result").innerHTML ='The result is = ' +  sumation;
+
+}
+// let sumation = sum ()
+
+// // console.log(sum());
+// console.log(sumation);
+// document.getElementById("result").innerHTML ='The result is = ' +  sumation;
+
+
+
+
+
+
+
+
+
+
+
+// ===============
+
+
+// function sum (){
+//     let x = document.getElementById ("number1").value;
+//     let y = document.getElementById ("number2").value;
+    
+//     parseFloat(x);
+//     parseFloat(y);
+//     let result =  parseFloat (x) + parseFloat(y);
+
+//     document.getElementById("result").innerHTML = "The Result is =" + result; + "Taka"
+// }
+
+
